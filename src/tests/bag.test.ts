441b49c2b1939123b5dcd54cc4e1cd7ab9ba6594
seophohoho <seophohoho@gmail.com>
2025-02-08 18:31:00 +0900

    test(bag): update test input data

--- src/tests/bag.test.ts
+++ src/tests/bag.test.ts
@@ -1,4 +1,4 @@
-import { Bag } from '../object/bag';
+import { Bag } from '../storage/bag';
 import { ITEM } from '../enums/item';
 
 describe('Bag Tests', () => {
@@ -6,40 +6,43 @@ describe('Bag Tests', () => {
 
   beforeEach(() => {
     bag = new Bag();
-    bag.setup();
   });
 
   test('아이템 추가 테스트.', () => {
     bag.addItems('001', 3);
     expect(Object.keys(bag.getPockets(ITEM.POKEBALL))).toContain('001');
-    expect(bag.getItem('001')?.getStock()).toBe(7);
+    expect(bag.getItem('001')?.getStock()).toBe(3);
   });
 
   test('아이템 사용 후 stock 감소 테스트1.', () => {
-    bag.useItem('001', 4);
-    expect(bag.getItem('001')).toBeUndefined();
+    bag.addItems('001', 3);
+    bag.useItem('001', 1);
+    expect(bag.getItem('001')?.getStock()).toBe(2);
   });
 
   test('아이템 사용 후 stock 감소 테스트2.', () => {
-    bag.addItems('006', 4);
+    bag.addItems('006', 3);
     bag.useItem('006', 1);
-    expect(bag.getItem('006')?.getStock()).toBe(3);
+    expect(bag.getItem('006')?.getStock()).toBe(2);
 
-    bag.useItem('006', 2);
+    bag.useItem('006', 1);
     expect(bag.getItem('006')?.getStock()).toBe(1);
 
-    bag.useItem('006', 2);
+    bag.useItem('006', 1);
     expect(bag.getItem('006')?.getStock()).toBeUndefined();
   });
 
   test('아이템 등록 테스트.', () => {
+    bag.addItems('001');
     bag.registerItem('001', 1);
     expect(bag.getItem('001')?.getRegister()).toBe(1);
 
+    bag.addItems('002');
     bag.registerItem('002', 1);
     expect(bag.getItem('002')?.getRegister()).toBe(1);
     expect(bag.getItem('001')?.getRegister()).toBeNull();
 
+    bag.addItems('003');
     bag.registerItem('003', 1);
     expect(bag.getItem('003')?.getRegister()).toBe(1);
     bag.registerItem('003', 2);
