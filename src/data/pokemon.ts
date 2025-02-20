9eb508dad9ea47edf0d33b308c42fa4f06d1d612
seophohoho <seop0937@gmail.com>
2025-02-20 10:28:51 +0900

    feat(pokebox-ui): add pokemon types ui on pokebox-ui

--- src/data/pokemon.ts
+++ src/data/pokemon.ts
@@ -6,8 +6,8 @@ export const MAX_POKEDEX = 9;
 export interface PokemonInfo {
   rank: POKEMON_RANK;
   nextEvole: string | null;
-  type1: TYPE;
-  type2: TYPE;
+  type1: TYPE | null;
+  type2: TYPE | null;
   size: 1 | 2 | 3 | 4;
 }
 
@@ -16,12 +16,12 @@ const initial: Record<string, PokemonInfo> = {
   '001': { rank: POKEMON_RANK.NORMAL, nextEvole: '002', type1: TYPE.GRASS, type2: TYPE.POISON, size: 1 },
   '002': { rank: POKEMON_RANK.NORMAL, nextEvole: '003', type1: TYPE.GRASS, type2: TYPE.POISON, size: 2 },
   '003': { rank: POKEMON_RANK.NORMAL, nextEvole: null, type1: TYPE.GRASS, type2: TYPE.POISON, size: 3 },
-  '004': { rank: POKEMON_RANK.NORMAL, nextEvole: '005', type1: TYPE.FIRE, type2: TYPE.NONE, size: 1 },
-  '005': { rank: POKEMON_RANK.NORMAL, nextEvole: '006', type1: TYPE.FIRE, type2: TYPE.NONE, size: 2 },
+  '004': { rank: POKEMON_RANK.NORMAL, nextEvole: '005', type1: TYPE.FIRE, type2: null, size: 1 },
+  '005': { rank: POKEMON_RANK.NORMAL, nextEvole: '006', type1: TYPE.FIRE, type2: null, size: 2 },
   '006': { rank: POKEMON_RANK.NORMAL, nextEvole: null, type1: TYPE.FIRE, type2: TYPE.FLYING, size: 3 },
-  '007': { rank: POKEMON_RANK.NORMAL, nextEvole: '008', type1: TYPE.WATER, type2: TYPE.NONE, size: 1 },
-  '008': { rank: POKEMON_RANK.NORMAL, nextEvole: '009', type1: TYPE.WATER, type2: TYPE.NONE, size: 2 },
-  '009': { rank: POKEMON_RANK.NORMAL, nextEvole: null, type1: TYPE.WATER, type2: TYPE.NONE, size: 3 },
+  '007': { rank: POKEMON_RANK.NORMAL, nextEvole: '008', type1: TYPE.WATER, type2: null, size: 1 },
+  '008': { rank: POKEMON_RANK.NORMAL, nextEvole: '009', type1: TYPE.WATER, type2: null, size: 2 },
+  '009': { rank: POKEMON_RANK.NORMAL, nextEvole: null, type1: TYPE.WATER, type2: null, size: 3 },
 };
 
 export const pokemonData = new Proxy(initial, {
