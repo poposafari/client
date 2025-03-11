9223993b52caa38f9556b6bec102fc4c1dd9aa94
seophohoho <seophohoho@gmail.com>
2025-03-11 19:20:04 +0900

    feat(overworld-ui): implement wild Pokémon spawning logic on water

--- src/ui/overworld-ui.ts
+++ src/ui/overworld-ui.ts
@@ -74,6 +74,7 @@ export class OverworldUi extends Ui {
   private playerInitPos: PlayerInitPos = { x: 0, y: 0, px: 0, py: 0 };
   private playerObj!: PlayerObject;
 
+  // private readonly MapScale: number = 3;
   private readonly MapScale: number = 1.5;
 
   constructor(scene: InGameScene, mode: OverworldMode, key: string) {
