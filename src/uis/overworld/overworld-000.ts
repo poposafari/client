cd61600f78421e863bea9b4d59695a58bc79d1b2
seophohoho <seop0937@gmail.com>
2025-06-11 15:37:33 +0900

    chore(map): modify tilemap layer positions and decorations

--- src/uis/overworld/overworld-000.ts
+++ src/uis/overworld/overworld-000.ts
@@ -18,14 +18,16 @@ export class Overworld000 extends OverworldUi {
   }
 
   show(): void {
-    this.map.setup(TEXTURE.OVERWORLD_000, [TEXTURE.TILE_FLOOR, TEXTURE.TILE_OBJECT]);
+    this.map.setup(TEXTURE.OVERWORLD_000, [TEXTURE.TILE_FLOOR, TEXTURE.TILE_OBJECT, TEXTURE.TILE_EDGE]);
     this.map.setLayer(0, TEXTURE.TILE_FLOOR, DEPTH.GROUND);
-    this.map.setLayer(1, TEXTURE.TILE_OBJECT, DEPTH.GROUND + 1);
-    this.map.setForegroundLayer(2, [TEXTURE.TILE_FLOOR, TEXTURE.TILE_OBJECT], DEPTH.FOREGROND);
+    this.map.setLayer(1, TEXTURE.TILE_FLOOR, DEPTH.GROUND);
+    this.map.setLayer(2, TEXTURE.TILE_EDGE, DEPTH.GROUND + 1);
+    this.map.setLayer(3, TEXTURE.TILE_OBJECT, DEPTH.GROUND + 1);
+    this.map.setForegroundLayer(4, [TEXTURE.TILE_FLOOR, TEXTURE.TILE_OBJECT], DEPTH.FOREGROND);
 
-    this.npc.setup(`npc001`, 11, 8, OVERWORLD_TYPE.PLAZA, 'talk');
-    this.npc.setup(`npc002`, 14, 8, OVERWORLD_TYPE.PLAZA, 'talk');
-    this.npc.setup(`npc000`, 8, 8, OVERWORLD_TYPE.PLAZA, 'talk');
+    this.npc.setup(`npc001`, 1, 2, OVERWORLD_TYPE.PLAZA, 'talk');
+    this.npc.setup(`npc002`, 2, 2, OVERWORLD_TYPE.PLAZA, 'talk');
+    this.npc.setup(`npc000`, 3, 2, OVERWORLD_TYPE.PLAZA, 'talk');
 
     super.show();
 
